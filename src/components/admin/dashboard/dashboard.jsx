39880b9a615f61399e70f5299ae1a79d9b3418e2
Giang <nggiang141@gmail.com>
2023-07-20 08:36:48 +0700

    feat

--- src/components/admin/dashboard/dashboard.jsx
+++ src/components/admin/dashboard/dashboard.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
+import {
+  CDBSidebar,
+  CDBSidebarContent,
+  CDBSidebarFooter,
+  CDBSidebarHeader,
+  CDBSidebarMenu,
+  CDBSidebarMenuItem,
+} from 'cdbreact';  
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
+  
   return (
-    <div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-      <div>a</div>
-    </div>
+    <div className="sidebar-bar">
+    <CDBSidebar className="cd-sidebar" >
+      <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
+      </CDBSidebarHeader>
+      <CDBSidebarFooter className="cdsidebar-footer">
+        <div className="board-calender">
+          <button type="button" value='Boards' onClick={() => {
+            navigate('/home/boards');
+          }} > Board</button>
+          <button type="button" value='Calendar' onClick={() => {
+            navigate('/home/calendar');
+          }} >Calendar</button>
+          <button type="button" value='Calendar' onClick={() => {
+            navigate('/home/calendar');
+          }} >Calendar</button>
+          <button type="button" value='Calendar' onClick={() => {
+            navigate('/home/calendar');
+          }} >Calendar</button>
+          <button type="button" value='Calendar' onClick={() => {
+            navigate('/home/calendar');
+          }} >Calendar</button>
+          <button type="button" value='Calendar' onClick={() => {
+            navigate('/home/calendar');
+          }} >Calendar</button>
+        </div>
+      </CDBSidebarFooter>
+
+    </CDBSidebar>
+  </div>
     )
 }
 
